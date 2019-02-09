c43a13001d5ddcdd9d32d55bf5a8b6e08f0890a5
Eric Kean <kreut@hotmail.com>
2019-02-09 14:25:10 -0800

    2. Transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,37 @@ pragma solidity ^0.5.0;
 
 contract DappToken {
   uint public totalSupply;
+  string public name;
+  string public symbol;
+  string public standard;
 
-  constructor() public {
-    totalSupply = 1000000;
+  mapping(address => uint) public balanceOf;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint _value
+    );
+
+  constructor(uint _initialSupply) public {
+    name = "Dapp Token";
+    symbol = "DAPP";
+    standard = "DAPP Token version v1.0";
+
+    totalSupply = _initialSupply;
+    balanceOf[msg.sender] = totalSupply;
   }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value, "not enough tokens to transfer");
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+    }
+
+
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,10 +1,52 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract("DappToken", accounts => {
-  it("sets the total supply upon deployment", async () =>
-    {
-      let tokenInstance = await DappToken.deployed();
-      let totalSupply = await tokenInstance.totalSupply();
-      assert.equal(1000000,totalSupply.toNumber(), "it correctly");
+  let tokenInstance;
+    beforeEach('setup contract for each test', async function () {
+        tokenInstance = await DappToken.deployed();
     });
+
+  it("initializes the contract with the correct values", async () =>{
+    let name = await tokenInstance.name();
+    assert.equal("Dapp Token", name, "it returns the correct name");
+
+    let symbol = await tokenInstance.symbol();
+    assert.equal("DAPP", symbol, "it returns the correct symbol");
+
+    let standard = await tokenInstance.standard();
+    assert.equal("DAPP Token version v1.0", standard, "it returns the correct standard");
+  });
+
+  it("allocates initial amount to 1,000,000", async () =>
+    {
+      let totalSupply = await tokenInstance.totalSupply();
+      assert.equal(1000000, totalSupply.toNumber(), "it correctly set the total supply to 1,000,000");
+
+      let adminBalance = await tokenInstance.balanceOf(accounts[0]);
+      assert.equal(1000000, adminBalance.toNumber(), "it correctly gives the admin the initial supply");
+    });
+
+  it("transfers token ownership", async () => {
+    let success = await tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0]});
+    assert.equal(success, true, 'it returns true');
+
+    try {
+      await tokenInstance.transfer.call(accounts[1], 1000000000);
+    } catch(error) {
+      assert(error.message.indexOf('not enough tokens to transfer') >=0, 'not enough tokens to transfer');
+    }
+    let receipt = await tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'triggers the transfer event');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account where the tokens are transferred from');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account where the tokens are transferred from');
+    assert.equal(receipt.logs[0].args._value.toNumber(), 250000, 'logs the value transferred');
+
+    let balance_1 = await tokenInstance.balanceOf(accounts[1]);
+    let balance_0 = await tokenInstance.balanceOf(accounts[0]);
+
+    assert.equal(balance_1.toNumber(), 250000, "adds the amount to new account");
+    assert.equal(balance_0.toNumber(), 750000, "subtracts the amount from the old account");
+  });
+
 });
