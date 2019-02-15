a9a7dcd04346dd5bf08f6f2c3db4dc4e7b7a2fc0
Eric Kean <kreut@hotmail.com>
2019-02-15 07:13:06 -0800

    6. Deploy contracts

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "lesson reform tornado smoke hawk slice remind occur axis trap acoustic angry";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: "7545",
       network_id: "*"
-    }
+    },
+    rinkeby: {
+      provider: function() {
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/a4827a64d92a4f2bae233502e5b6066a");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+  }
   }
 }
